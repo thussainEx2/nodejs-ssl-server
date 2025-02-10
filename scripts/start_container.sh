36c664eae9443ef025f472aeb1760e59b4068dc2
Talib Hassan <thassan@ex2india.com>
2025-02-10 15:46:43 +0530

    Added ECR Login Details in Start_Container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# AWS ECR login
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 430118853571.dkr.ecr.ap-south-1.amazonaws.com
+
+# Pull the latest image from ECR
 docker pull 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:latest
 
-# Run the Docker image as a container
-docker run -d -p 3000:3000 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:latest
+# Stop and remove any running container with the same name
+docker stop sample-node-app || true
+docker rm sample-node-app || true
+
+# Run the container
+docker run -d -p 80:3000 --name sample-node-app 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:latest
