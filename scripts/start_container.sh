a7fe6c1f7c8a4e6b1ca1f66033a20caa7ad9bd6c
Talib Hassan <thassan@ex2india.com>
2025-02-10 12:45:35 +0530

    Added AppSpec.yaml

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:20250210070952
+
+# Run the Docker image as a container
+docker run -d -p 3000:3000 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:20250210070952
