21faaeefdb307dabc63307a478800b113b516a00
Talib Hassan <thassan@ex2india.com>
2025-02-10 14:41:59 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:20250210070952
+docker pull 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:20250210091044
 
 # Run the Docker image as a container
-docker run -d -p 3000:3000 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:20250210070952
+docker run -d -p 3000:3000 430118853571.dkr.ecr.ap-south-1.amazonaws.com/sample-node-app:20250210091044
